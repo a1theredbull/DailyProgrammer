0a4263be827c3899c0ddc00b18fda2ddea695002
Alexander Chau <chau.time@yahoo.com>
2012-10-25 19:55:34 -0300

    Update InfiniteMonkeyTheorem/InfiniteMonkeyTheorem.py

--- InfiniteMonkeyTheorem/InfiniteMonkeyTheorem.py
+++ InfiniteMonkeyTheorem/InfiniteMonkeyTheorem.py
@@ -5,8 +5,6 @@ def print_monkey_doc(signum, frame):
 	print(written_by_monkey)
 	sys.exit(0)
 
-signal.signal(signal.SIGINT, print_monkey_doc)
-
 print('Theorem: If a monkey randomly mashes on a keyboard for an infinite amount of time, he/she will eventually' +
  ' write any piece of literature(i.e. Hamlet, but astronomically small chance).\n')
 
@@ -20,6 +18,7 @@ letters = 'aaabbccddeeeffgghhiiijkllmmnnoooppqrrssttuuuvwwxyyz           '
 gibberish = ''
 
 print('Monkey is about to mash the keyboard...press Ctrl+C to exit!')
+signal.signal(signal.SIGINT, print_monkey_doc)
 print('Be patient: Each valid word may take thousands of monkey attempts.')
 written_by_monkey = ''
 count = 0
